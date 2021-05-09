d91f064569faa19edaf8e7b7e4977ec23cd9cf7b
alexfullalove <alexanderfullalove@gmail.com>
2021-05-09 21:22:29 +0100

    dist updated

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.createFormData = void 0;
+var houses_json_1 = __importDefault(require("./data/houses.json"));
+var heat_pumps_json_1 = __importDefault(require("./data/heat-pumps.json"));
+exports.createFormData = function (houseData, heatPumpData) {
+    return "house";
+};
+exports.createFormData(houses_json_1.default, heat_pumps_json_1.default);
 //# sourceMappingURL=index.js.map
