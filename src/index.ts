7e1794e2d8d11ef425313b559db2a4194c891423
alexfullalove <alexanderfullalove@gmail.com>
2021-05-09 21:22:19 +0100

    createFormData function added

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import houses from "./data/houses.json";
 import heatPumps from "./data/heat-pumps.json";
+import { Houses } from "./models/Houses";
+import { HeatPumps } from "./models/HeatPumps";
+
+export const createFormData = (
+  houseData: Houses[],
+  heatPumpData: HeatPumps[]
+): string => {
+  return "house";
+};
+
+createFormData(houses, heatPumps);
